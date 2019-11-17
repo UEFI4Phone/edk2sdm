fd3b79140d75a26f2f78533e1865b746d96cfa3d
evsio0n <1344729087@qq.com>
2019-11-17 03:26:39 -0800

    Add automuild for making Kernel zImage

--- build.sh
+++ build.sh
@@ -10,3 +10,4 @@ export WORKSPACE=$PWD/workspace
 make -C ./edk2/BaseTools
 GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p XiaomiMI6Pkg/XiaomiMI6Pkg.dsc
 gzip -9nkc < workspace/Build/XiaomiMI6Pkg/DEBUG_GCC5/FV/XIAOMIMI6PKG_UEFI.fd >uefi.img
+cat uefi.img devicetree.dtb >> boot.img-zImage
