9146110106617d561c67a89370401aea0ea9a23f
evsio0n <1344729087@qq.com>
2019-12-19 13:21:27 +0000

    取消隱式Build FD

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ sudo apt update
 sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu bc
 curdir="$PWD"
 cd "$curdir"
+export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
 export PACKAGES_PATH=$PWD/edk2:$PWD/edk2-platforms:$PWD/Sdm835
 export WORKSPACE=$PWD/workspace
 . ./edk2/edksetup.sh > /dev/null
@@ -17,7 +18,7 @@ make -C ./edk2/BaseTools > /dev/null
 clear
 echo "[Evsio0n]		Now make FD Images..."
 sleep 1
-./tools/makefd.sh > /dev/null 2>&1
+./tools/makefd.sh > /dev/null 
 #GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p XiaomiMI6Pkg/XiaomiMI6Pkg.dsc
 echo "[Evsio0n]		Done!.............."
 sleep 2
