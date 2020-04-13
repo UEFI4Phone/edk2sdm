b87cb2e4b827963e5b231ffa1672c2db6982a362
Evsio0n <1344729087@qq.com>
2020-04-13 18:32:46 +0800

    Update azure-build.sh

--- azure-build.sh
+++ azure-build.sh
@@ -6,4 +6,10 @@ curdir="$PWD"
 cd "$curdir"
 export PACKAGES_PATH=$PWD/edk2:$PWD/edk2-platforms:$PWD/Sdm835
 export WORKSPACE=$PWD/workspace
-./build.sh
+. ./edk2/edksetup.sh
+make -C ./edk2/BaseTools
+. ./edk2/edksetup.sh
+GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p SamsungS8Pkg/SamsungS8Pkg.dsc
+gzip -9nkc < workspace/Build/SamsungS8Pkg/DEBUG_GCC5/FV/SAMSUNGS8PKG_UEFI.fd >uefi.img
+cat uefi.img devicetree.dtb >> ./tools/split_img/boot.img-zImage
+bash ./tools/repackimg.sh
