030464f74fbf493e17f15c2bd0c7e4d21025597a
Evsio0n <1344729087@qq.com>
2020-04-24 08:22:09 +0800

    Update azure-build.sh

--- azure-build.sh
+++ azure-build.sh
@@ -4,6 +4,7 @@ sudo apt update
 sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu bc
 curdir="$PWD"
 cd "$curdir"
+sudo chmod a+x * -R
 export PACKAGES_PATH=$PWD/edk2:$PWD/edk2-platforms:$PWD/Sdm835
 export WORKSPACE=$PWD/workspace
 . ./edk2/edksetup.sh
